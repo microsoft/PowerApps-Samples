881808b7d0e798edd12e17a81228d872087bce96
Jim Daly <Jim.Daly@microsoft.com>
2018-04-23 13:53:01 -0700

    Completed CRUD-Dynamic-Entity Sample

--- cds/orgsvc/C#/CRUD-Dynamic-Entity/CRUD-Dynamic-Entity/SampleProgram.cs
+++ cds/orgsvc/C#/CRUD-Dynamic-Entity/CRUD-Dynamic-Entity/SampleProgram.cs
@@ -3,53 +3,100 @@ using Microsoft.Xrm.Tooling.Connector;
 using System;
 using System.Configuration;
 using System.ServiceModel;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace PowerApps.Samples
 {
-    partial class SampleProgram
+    public class SampleProgram
     {
         static void Main(string[] args)
         {
-            string connectionString = string.Empty;
-            //Verify common-data-service/App.config contains a valid connection string named 'Connect'
+
             try
             {
-                connectionString = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
-            }
-            catch (NullReferenceException)
-            {
-                Console.WriteLine("You must set connection data in common-data-service/App.config before running this sample.");                
-            }
-            
-
-            try {
-                using (CrmServiceClient csc = new CrmServiceClient(connectionString))
+                //You must specify connection information in cds/App.config to run this sample.
+                using (CrmServiceClient csc = new CrmServiceClient(GetConnectionStringFromAppConfig("Connect")))
                 {
                     if (csc.IsReady)
                     {
                         IOrganizationService service = csc.OrganizationServiceProxy;
 
-                        //Add code here
+                        #region Sample Code
                         //////////////////////////////////////////////
-                        Entity account = new Entity("account");
-                        account["name"] = "Test Account";
+                        #region Demonstrate
+                        
+                        // Instantiate an account object.
 
-                        //User service methods directly:
-                        // Guid accountid = service.Create(account);
+                        Entity newAccount = new Entity("account");
 
-                        //Use method defined in SampleMethods.cs
-                        Guid accountid = CreateEntity(service, account);
+                        // Set the required attributes. For account, only the name is required. 
 
+                        newAccount["name"] = "Fourth Coffee";
 
+                        //Set any other attribute values.
 
+                        newAccount["address2_postalcode"] = "98074";
+
+                        // Create an account record named Fourth Coffee.
+
+                        Guid accountid = service.Create(newAccount);
+                      
+                        Console.WriteLine("Created {0} entity named {1}.", newAccount.LogicalName, newAccount["name"]);
+
+                        // Create a column set to define which attributes should be retrieved.                       
+                        ColumnSet attributes = new ColumnSet("name", "ownerid");
+
+                        // Retrieve the account and its name and ownerid attributes.
+                        newAccount = service.Retrieve(newAccount.LogicalName, accountid, attributes);
+
+                        Console.WriteLine("Retrieved Entity");
+                        
+                        /*
+                        IMPORTANT: 
+                        Do not update an entity using a retrieved entity instance.
+                        Always instantiate a new Entity and 
+                        set the primary key value to match the entity you want to update.
+                        Only set the attribute values you are changing.
+                        */
+
+                        Entity accountToUpdate = new Entity("account");
+                        accountToUpdate["accountid"] = accountid;
+
+                        // Update the address 1 postal code attribute.
+                        accountToUpdate["address1_postalcode"] = "98052";
+
+                        // The address 2 postal code was set accidentally, so set it to null.
+                        accountToUpdate["address2_postalcode"] = null;
+
+                        // Shows use of Money.
+                        accountToUpdate["revenue"] = new Money(5000000);
+
+                        // Shows use of boolean.
+                        accountToUpdate["creditonhold"] = false;
+
+                        // Perform the update.
+                        service.Update(accountToUpdate);
+
+                        Console.WriteLine("Updated Entity");
+
+                        //Delete the entity
+                        service.Delete("account", accountid);
+
+                        Console.WriteLine("Deleted Entity");
+
+                        #endregion Demonstrate
                         //////////////////////////////////////////////
+                        #endregion Sample Code
+
                         Console.WriteLine("The sample completed successfully");
+                        return;
                     }
                     else
                     {
-                        if (csc.LastCrmError.Equals("Unable to Login to Dynamics CRM"))
+                        const string UNABLE_TO_LOGIN_ERROR = "Unable to Login to Dynamics CRM";
+                        if (csc.LastCrmError.Equals(UNABLE_TO_LOGIN_ERROR))
                         {
-                            Console.WriteLine("Check the connection string values in common-data-service/App.config.");
+                            Console.WriteLine("Check the connection string values in cds/App.config.");
                             throw new Exception(csc.LastCrmError);
                         }
                         else
@@ -105,13 +152,29 @@ namespace PowerApps.Samples
 
             finally
             {
-                
+
                 Console.WriteLine("Press <Enter> to exit.");
                 Console.ReadLine();
             }
 
-
-
+        }
+        /// <summary>
+        /// Gets a named connection string from App.config
+        /// </summary>
+        /// <param name="name">The name of the connection string to return</param>
+        /// <returns>The named connection string</returns>
+        static string GetConnectionStringFromAppConfig(string name)
+        {
+            //Verify cds/App.config contains a valid connection string with the name.
+            try
+            {
+                return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You must set connection data in cds/App.config before running this sample.");
+                return string.Empty;
+            }
         }
     }
 }
