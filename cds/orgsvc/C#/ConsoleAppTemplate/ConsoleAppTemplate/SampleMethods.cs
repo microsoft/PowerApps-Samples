692696aebcd94fc1f9698c79a24c9e91aeca0ba8
JimDaly <jim.daly@microsoft.com>
2018-05-12 15:37:39 -0700

    Added ConsoleAppTemplate project
    Minimum project with Generic name to show pattern for new Samples

---
+++ cds/orgsvc/C#/ConsoleAppTemplate/ConsoleAppTemplate/SampleMethods.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xrm.Tooling.Connector;
+
+namespace PowerApps.Samples
+{
+    public partial class SampleProgram
+    {
+
+        /// <summary>
+        /// Function to set up the sample.
+        /// </summary>
+        /// <param name="service">Specifies the service to connect to.</param>
+        private static void SetUpSample(CrmServiceClient service)
+        {
+
+        }
+        /// <summary>
+        /// Method to clean up any sample data
+        /// </summary>
+        /// <param name="service">Specifies the service to connect to.</param>
+        private static void CleanUpSample(CrmServiceClient service)
+        {
+            
+        }
+    }
+}
