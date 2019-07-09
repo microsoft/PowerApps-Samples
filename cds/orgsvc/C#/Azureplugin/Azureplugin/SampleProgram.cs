a8532a85b7ee4b1b341d2b18252525e590004434
NavaKiran <34038327+Nkrb@users.noreply.github.com>
2019-07-09 11:07:45 -0700

    Fixing Azure plugin sample

---
+++ cds/orgsvc/C#/Azureplugin/Azureplugin/SampleProgram.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerApps.Samples
+{
+    /// <summary>
+    /// A custom plug-in that can post the execution context of the current message to the Windows
+    /// Azure Service Bus. The plug-in also demonstrates tracing which assist with
+    /// debugging for plug-ins that are registered in the sandbox.
+    /// </summary>
+    /// <remarks>This sample requires that a service endpoint be created first, and its ID passed
+    /// to the plug-in constructor through the unsecure configuration parameter when the plug-in
+    /// step is registered.</remarks>
+    public sealed class SandboxPlugin : IPlugin
+    {
+        private Guid serviceEndpointId;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SandboxPlugin(string config)
+        {
+            if (String.IsNullOrEmpty(config) || !Guid.TryParse(config, out serviceEndpointId))
+            {
+                throw new InvalidPluginExecutionException("Service endpoint ID should be passed as config.");
+            }
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Retrieve the execution context.
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // Extract the tracing service.
+            ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+            if (tracingService == null)
+                throw new InvalidPluginExecutionException("Failed to retrieve the tracing service.");
+
+            IServiceEndpointNotificationService cloudService = (IServiceEndpointNotificationService)serviceProvider.GetService(typeof(IServiceEndpointNotificationService));
+            if (cloudService == null)
+                throw new InvalidPluginExecutionException("Failed to retrieve the service bus service.");
+
+            try
+            {
+                tracingService.Trace("Posting the execution context.");
+                string response = cloudService.Execute(new EntityReference("serviceendpoint", serviceEndpointId), context);
+                if (!String.IsNullOrEmpty(response))
+                {
+                    tracingService.Trace("Response = {0}", response);
+                }
+                tracingService.Trace("Done.");
+            }
+            catch (Exception e)
+            {
+                tracingService.Trace("Exception: {0}", e.ToString());
+                throw;
+            }
+        }
+    }
+}
