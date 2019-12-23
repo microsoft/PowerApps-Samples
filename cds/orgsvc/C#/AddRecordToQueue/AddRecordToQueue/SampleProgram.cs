56bb7bd38853a580e42d97fc3727b025de7e0434
NavaKiran <34038327+Nkrb@users.noreply.github.com>
2019-12-23 11:55:01 -0800

    Adding samples

---
+++ cds/orgsvc/C#/AddRecordToQueue/AddRecordToQueue/SampleProgram.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerApps.Samples
+{
+    public partial class SampleProgram
+    {
+        [STAThread] // Required to support the interactive login experience
+        static void Main(string[] args)
+        {
+            CrmServiceClient service = null;
+            try
+            {
+                service = SampleHelpers.Connect("Connect");
+                if (service.IsReady)
+                {
+                    // Create any entity records that the demonstration code requires
+                    SetUpSample(service);
+                    #region Demonstrate
+                    Guid CurrentUserId = ((WhoAmIResponse)service.Execute(new WhoAmIRequest())).UserId;
+
+                    // Get known private queues for the user 
+                    // by using RetrieveUserQueuesRequest message.
+                    var retrieveUserQueuesRequest = new RetrieveUserQueuesRequest
+                    {
+                        UserId = CurrentUserId,
+                        IncludePublic = true
+                    };
+                    var retrieveUserQueuesResponse =
+                        (RetrieveUserQueuesResponse)service.Execute(retrieveUserQueuesRequest);
+                    EntityCollection queues = (EntityCollection)retrieveUserQueuesResponse.EntityCollection;
+
+                    Guid sourceQueueId = new Guid();
+                    Guid destinationQueueId = new Guid();
+
+                    foreach (Entity entity in queues.Entities)
+                    {
+                        var queue = (Queue)entity;
+                        switch (queue.Name)
+                        {
+                            case "Source Queue":
+                                sourceQueueId = queue.Id;
+                                break;
+                            case "Destination Queue":
+                                destinationQueueId = queue.Id;
+                                break;
+                        }
+                    }
+
+                    // Move a record from a source queue to a destination queue
+                    // by using the AddToQueue request message.
+                    var routeRequest = new AddToQueueRequest
+                    {
+                        SourceQueueId = sourceQueueId,
+                        Target = new EntityReference(Letter.EntityLogicalName, _letterId),
+                        DestinationQueueId = destinationQueueId
+                    };
+
+                    // Execute the Request
+                    service.Execute(routeRequest);
+
+                    Console.WriteLine(@"The letter record has been moved to a new queue.");
+                    #endregion Demonstrate
+
+                    #region Clean up
+                    CleanUpSample(service);
+                    #endregion Clean up
+                }
+                else
+                {
+                    const string UNABLE_TO_LOGIN_ERROR = "Unable to Login to Common Data Service";
+                    if (service.LastCrmError.Equals(UNABLE_TO_LOGIN_ERROR))
+                    {
+                        Console.WriteLine("Check the connection string values in cds/App.config.");
+                        throw new Exception(service.LastCrmError);
+                    }
+                    else
+                    {
+                        throw service.LastCrmException;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SampleHelpers.HandleException(ex);
+            }
+
+            finally
+            {
+                if (service != null)
+                    service.Dispose();
+
+                Console.WriteLine("Press <Enter> to exit.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ cds/orgsvc/C#/InitializeRecordFromExisting/InitializeRecordFromExisting/SampleMethods.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerApps.Samples
+{
+    public partial class SampleProgram
+    {
+
+        private static Account _initialAccount;
+        private static Opportunity _initialOpportunity;
+        private static bool prompt = true;
+        /// <summary>
+        /// Function to set up the sample.
+        /// </summary>
+        /// <param name="service">Specifies the service to connect to.</param>
+        /// 
+        private static void SetUpSample(CrmServiceClient service)
+        {
+            // Check that the current version is greater than the minimum version
+            if (!SampleHelpers.CheckVersion(service, new Version("7.1.0.0")))
+            {
+                //The environment version is lower than version 7.1.0.0
+                return;
+            }
+
+            CreateRequiredRecords(service);
+        }
+
+        private static void CleanUpSample(CrmServiceClient service)
+        {
+            DeleteRequiredRecords(service, prompt);
+        }
+
+        /// <summary>
+        /// This method creates any entity records that this sample requires.
+        /// Creates the email activity.
+        /// </summary>
+        public static void CreateRequiredRecords(CrmServiceClient service)
+        {
+
+            _initialAccount = new Account()
+            {
+                Name = "Contoso, Ltd"
+            };
+            _initialAccount.Id = service.Create(_initialAccount);
+            Console.WriteLine("  Created initial Account (Name={0})",
+                _initialAccount.Name);
+
+            _initialOpportunity = new Opportunity()
+            {
+                Subject = "A Sample Lead",
+                LastName = "Wilcox",
+                FirstName = "Colin",
+            };
+            _initialLead.Id = service.Create(_initialLead);
+            Console.WriteLine("  Created initial Lead (Subject={0}, Name={1} {2})",
+                _initialLead.Subject,
+                _initialLead.FirstName,
+                _initialLead.LastName);
+            Console.WriteLine("Required records have been created.");
+        }
+
+
+        /// <summary>
+        /// Deletes the custom entity record that was created for this sample.
+        /// <param name="prompt">Indicates whether to prompt the user 
+        /// to delete the entity created in this sample.</param>
+        /// </summary>
+        public static void DeleteRequiredRecords(CrmServiceClient service, bool prompt)
+        {
+            bool deleteRecords = true;
+
+            if (prompt)
+            {
+                Console.WriteLine("\nDo you want these entity records deleted? (y/n)");
+                String answer = Console.ReadLine();
+
+                deleteRecords = (answer.StartsWith("y") || answer.StartsWith("Y"));
+            }
+
+            if (deleteRecords)
+            {
+
+                service.Delete(Account.EntityLogicalName, _initialAccount.Id);
+                service.Delete(Lead.EntityLogicalName, _initialLead.Id);
+                Console.WriteLine("Entity records have been deleted.");
+            }
+        }
+
+    }
+}
