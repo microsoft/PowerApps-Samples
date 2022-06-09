4408b4ce93c672013de0b7b14180556da7c74869
Weiying Chen <104045722+weiyingmichellechen@users.noreply.github.com>
2022-06-08 19:30:07 -0700

    Add file upload and download sample using webapi

---
+++ cds/webapi/C#/UploadDownloadFiles/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Http;
+using System.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace PowerApps.Samples
+{
+    partial class Program
+    {
+        private static string _accountId;
+        private static string _fileAttributeLogicalName;
+        private static string _uploadFileName;
+        private static string _downloadFolder;
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Get configuration data from App.config connectionStrings
+                string connectionString = ConfigurationManager.ConnectionStrings["Connect"].ConnectionString;
+                Console.Write("Enter an existing file attribute column logical name, like mc_file1:  ");
+                _fileAttributeLogicalName = Console.ReadLine();
+
+                Console.Write("Enter a file to be uploaded, like c:\\UploadedFiles\\document.pdf:   ");
+                _uploadFileName = Console.ReadLine();
+
+                Console.Write("Enter a download folder, like c:\\DownloadedFiles\\:   ");
+                _downloadFolder = Console.ReadLine();
+
+                using (HttpClient client = SampleHelpers.GetHttpClient(connectionString, SampleHelpers.clientId, SampleHelpers.redirectUrl))
+                {
+                    var urlForGetAccountId = $"{client.BaseAddress}accounts?$select=name,accountid&$top=10";
+
+                    HttpResponseMessage response = client.GetAsync(urlForGetAccountId,
+                            HttpCompletionOption.ResponseHeadersRead).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        JObject body = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+                        _accountId = body["value"][0]["accountid"].ToString();
+
+                        Console.WriteLine($"Uploading {_uploadFileName} in progress ...");
+                        UploadFile(client, _fileAttributeLogicalName, _uploadFileName);
+                        Console.WriteLine($"Upload succeeded");
+
+                        Console.WriteLine($"Downloading {_uploadFileName} in progress ...");
+                        DownloadFile(client, _fileAttributeLogicalName,_downloadFolder);
+                        Console.WriteLine($"Download succeeded");
+
+                        Console.WriteLine($"Deleting {_fileAttributeLogicalName} in progress ...");
+                        DeleteFile(client, _fileAttributeLogicalName);
+                        Console.WriteLine($"Delete {_fileAttributeLogicalName} succeeded.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                SampleHelpers.DisplayException(ex);
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadLine();
+            }           
+        }
+    }
+}
---
+++ cds/webapi/C#/UploadDownloadFiles/UploadDownload.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+
+namespace PowerApps.Samples
+{
+    /// <summary>
+    /// Partial class
+    /// </summary>
+    partial class Program
+    {
+
+        /// <summary>
+        /// Upload a file
+        /// </summary>
+        /// <param name="client">The HttpClient object to use</param>
+        /// <param name="logicalName">The file column logical name</param>
+        /// <param name="fileName">The name of the uploaded file</param>
+        /// <exception cref="Exception">Exception thrown</exception>
+        public static void UploadFile(HttpClient client, string logicalName, string fileName)
+        {
+            var fileUrl = $"{client.BaseAddress}accounts({_accountId})/{logicalName}";
+            var fileStream = File.OpenRead(fileName);
+            var request = new HttpRequestMessage()
+            {
+                Method = new HttpMethod("Patch"),
+                RequestUri = new Uri(fileUrl)
+            };
+            request.Content = new StreamContent(fileStream);
+            request.Content.Headers.Add("Content-Type", "application/octet-stream");
+            request.Content.Headers.Add("x-ms-file-name", Path.GetFileName(fileName));
+            HttpResponseMessage response = client.SendAsync(request)?.Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"File {fileName} successfully uploaded.");
+            }
+            else
+            {
+                throw new Exception($"The UploadFile request failed with a status of '{response.ReasonPhrase}')");
+            }
+        }
+
+
+        /// <summary>
+        /// Download a file
+        /// </summary>
+        /// <param name="client">The HttpClient object to use</param>
+        /// <param name="logicalName">The file column logical name</param>
+        /// <param name="downloadFolder">The download folder</param>
+        /// <exception cref="Exception">Exception thrown</exception>
+        public static void DownloadFile(HttpClient client, string logicalName, string downloadFolder)
+        {
+            var fileUrl = $"{client.BaseAddress}accounts({_accountId})/{logicalName}/$value?size=full";
+            var request = new HttpRequestMessage()
+            {
+                Method = new HttpMethod("Get"),
+                RequestUri = new Uri(fileUrl)
+            };
+
+            var response = client.SendAsync(request)?.Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content.ReadAsByteArrayAsync().Result;
+                var downloadFilePath = Path.Combine(downloadFolder, $"{DateTime.Now.ToString("yyyyMMddTHHmmss")}{Path.GetFileName(_uploadFileName)}");
+                Console.WriteLine($"Saving download in {downloadFilePath}");
+                File.WriteAllBytes(downloadFilePath, responseContent);
+            }
+            else
+            {
+                throw new Exception($"The DownloadFile request failed with a status of '{response.ReasonPhrase}')");
+            }
+        }
+
+
+
+        /// <summary>
+        /// Delete a file
+        /// </summary>
+        /// <param name="client">The HttpClient object to use</param>
+        /// <param name="logicalName">The file attribute logical name</param>
+        /// <exception cref="Exception">Exception thrown</exception>
+        public static void DeleteFile(HttpClient client, string logicalName)
+        {
+            var fileUrl = $"{client.BaseAddress}accounts({_accountId})/{logicalName}";
+            var request = new HttpRequestMessage()
+            {
+                Method = new HttpMethod("Delete"),
+                RequestUri = new Uri(fileUrl)
+            };
+
+            var response = client.SendAsync(request)?.Result;
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                throw new Exception($"The DeleteFile request failed with a status of '{response.ReasonPhrase}'");
+            }
+        }
+    }
+}
