61bd1674fd1fc2a89d7bcaae805e4ff96c990ddd
Jim Daly <jim.daly@microsoft.com>
2023-05-25 14:14:12 -0700

    Initial commit

---
+++ dataverse/orgsvc/C#-NETCore/ElasticTableOperations/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+
+namespace PowerPlatform.Dataverse.CodeSamples
+{
+    class Program
+    {
+        /// <summary>
+        /// Contains the application's configuration settings. 
+        /// </summary>
+        IConfiguration Configuration { get; }
+
+
+        /// <summary>
+        /// Constructor. Loads the application configuration settings from a JSON file.
+        /// </summary>
+        Program()
+        {
+
+            // Get the path to the appsettings file. If the environment variable is set,
+            // use that file path. Otherwise, use the runtime folder's settings file.
+            string? path = Environment.GetEnvironmentVariable("DATAVERSE_APPSETTINGS");
+            if (path == null) path = "appsettings.json";
+
+            // Load the app's configuration settings from the JSON file.
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile(path, optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        static void Main()
+        {
+            Program app = new();
+
+            // Create a Dataverse service client using the default connection string.
+            ServiceClient serviceClient =
+                new(app.Configuration.GetConnectionString("default"));
+        }
+    }
+}
