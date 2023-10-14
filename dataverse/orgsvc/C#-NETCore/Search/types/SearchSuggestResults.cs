2c729bf1d62e118a512b5cb4545cb94b6db41bdc
Jim Daly <jim.daly@microsoft.com>
2023-10-13 18:44:17 -0700

    README completed

--- dataverse/orgsvc/C#-NETCore/Search/types/SearchSuggestResults.cs
+++ dataverse/orgsvc/C#-NETCore/Search/types/SearchSuggestResults.cs
@@ -3,6 +3,9 @@ using PowerApps.Samples.Search.Types;
 
 namespace PowerPlatform.Dataverse.CodeSamples.types
 {
+    /// <summary>
+    /// Contains the data from the searchsuggest response
+    /// </summary>
     class SearchSuggestResults
     {
         [JsonProperty(PropertyName = "Error")]
