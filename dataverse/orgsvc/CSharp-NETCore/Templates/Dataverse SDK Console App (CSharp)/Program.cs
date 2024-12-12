53b5abcdfbc4fcc3c95aefcb1483c79bbcbf6215
Winona Azure <v-wiazure@microsoft.com>
2024-12-12 13:55:30 -0800

    Finishing up changes in the Csharp-NETCore folder

---
+++ dataverse/orgsvc/CSharp-NETCore/Templates/Dataverse SDK Console App (CSharp)/Program.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+
+namespace PowerPlatform_Dataverse_CodeSamples
+{
+    internal class Program
+    {
+        // TODO Add your custom methods here for quick access
+
+
+        /// <summary>
+        /// Contains the application's configuration settings. 
+        /// </summary>
+        static IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Constructor. Loads the application settings from a JSON configuration file.
+        /// </summary>
+        static Program()
+        {
+            // Get the path to the appsettings file. If the environment variable is set,
+            // use that file path. Otherwise, use the runtime folder's settings file.
+            string? path = Environment.GetEnvironmentVariable("DATAVERSE_APPSETTINGS");
+            if (path == null) path = "appsettings.json";
+
+            // Load the app's configuration settings from the JSON file.
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile(path, optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        static void Main(string[] args)
+        {
+            // Entity name and reference collection.
+            Dictionary<string,EntityReference> entityStore;
+
+            // Create a Dataverse service client using the default connection string.
+            ServiceClient serviceClient =
+                new(Configuration.GetConnectionString("default"));
+
+            // Pre-create any table rows that Run() requires.
+            Setup(serviceClient, out entityStore);
+
+            // Execute the main logic of this program
+            Run(serviceClient, entityStore);
+
+            // Pause program execution before resource cleanup.
+            Console.WriteLine("Press any key to undo environment data changes.");
+            Console.ReadKey();
+
+            // In Dataverse, delete any created table rows and then dispose the service connection.
+            Cleanup(serviceClient, entityStore);
+            serviceClient.Dispose();
+        }
+
+        /// <summary>
+        /// Initializes any pre-existing data and resources required by the Run() method.
+        /// </summary>
+        /// <param name="service">Authenticated web service connection.</param>
+        /// <param name="entityStore">Entity name and reference collection.</param>
+        static public void Setup(IOrganizationService service, out Dictionary<string, 
+            EntityReference> entityStore)
+        {
+            entityStore = new Dictionary<string, EntityReference>();
+
+            // TODO Setup(): Create required pre-existing entities or resources as needed
+        }
+
+        /// <summary>
+        /// The main logic of this program being demonstrated.
+        /// </summary>
+        /// <param name="service">Authenticated web service connection.</param>
+        /// <param name="entityStore">Entity name and reference collection.</param>
+        /// <returns>True if successful; otherwise false.</returns>
+        static public bool Run(IOrganizationService service, 
+            Dictionary<string, EntityReference> entityStore)
+        {
+            // TODO Run(): add code that is being demonstrated
+
+            return false;
+        }
+
+        /// <summary>
+        /// Dispose of any data and resources created by the this program.
+        /// </summary>
+        /// <param name="service">Authenticated web service connection.</param>
+        /// <param name="entityStore">Entity name and reference collection.</param>
+        static public void Cleanup(ServiceClient service, 
+            Dictionary<string, EntityReference> entityStore)
+        {
+            // Do some checking of the passed parameter values.
+            if (service == null || service.IsReady == false)
+            {
+                Console.WriteLine("Cleanup(): web service connection is not available, cleanup aborted.");
+                return;
+            }
+
+            if (entityStore == null)
+            {
+                Console.WriteLine("Cleanup(): entref store collection is null, cleanup aborted.");
+                Console.WriteLine("Cleanup(): be sure to run Setup() prior to Cleanup().");
+                return;
+            }
+
+            // Collect the keys of entities to be deleted.
+            var keysToDelete = new List<string>(entityStore.Keys);
+            keysToDelete.Reverse();
+
+            // Delete in Dataverse each entity in the entity store.
+            foreach (var key in keysToDelete)
+            {
+                var entref = entityStore[key];
+                try
+                {
+                    service.Delete(entref.LogicalName, entref.Id);
+                    entityStore.Remove(key);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Cleanup(): exception deleting {key}\n\t{ex.Message}");
+                    continue;
+                }
+            }
+
+            // Output a list of entities that could not be deleted.
+            if (entityStore.Count > 0)
+            {
+                Console.WriteLine("Cleanup(): the following entities could not be deleted:");
+                foreach (var item in entityStore)
+                {
+                    Console.WriteLine($"Cleanup(): name={item.Key}, " +
+                        $"logical name={item.Value.LogicalName}, ID={item.Value.Id}");
+                }
+            }
+        }
+    }
+}
