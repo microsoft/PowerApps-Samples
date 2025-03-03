86046f22ca11d29df93dd39bfe9e13dda6f49605
Aashman <aashmangupta@gmail.com>
2025-03-03 14:50:56 -0500

    fix casing

--- powershell/connectors/HTTPWithMicrosoftEntraId/ManagePermissionGrant.ps1
+++ powershell/connectors/HTTPWithMicrosoftEntraId/ManagePermissionGrant.ps1
@@ -14,7 +14,7 @@ function Get-FirstPartyAppList{
 		[App]@{IsCommonlyUsedApp=$false;ApplicationName='ASM Campaign Servicing';AppId='0cb7b9ec-5336-483b-bc31-b15b5788de71'},
 		[App]@{IsCommonlyUsedApp=$false;ApplicationName='Azure Advanced Threat Protection';AppId='7b7531ad-5926-4f2d-8a1d-38495ad33e17'},
 		[App]@{IsCommonlyUsedApp=$false;ApplicationName='Azure Data Lake';AppId='e9f49c6b-5ce5-44c8-925d-015017e9f7ad'},
-  		[App]@{IsCommonlyUsedApp=$false;ApplicationName='Azure Devops';AppId='499b84ac-1321-427f-aa17-267ca6975798'},
+  		[App]@{IsCommonlyUsedApp=$false;ApplicationName='Azure DevOps';AppId='499b84ac-1321-427f-aa17-267ca6975798'},
 		[App]@{IsCommonlyUsedApp=$false;ApplicationName='Azure Lab Services Portal';AppId='835b2a73-6e10-4aa5-a979-21dfda45231c'},
 		[App]@{IsCommonlyUsedApp=$false;ApplicationName='Azure Portal';AppId='c44b4083-3bb0-49c1-b47d-974e53cbdf3c'},
 		[App]@{IsCommonlyUsedApp=$false;ApplicationName='AzureSupportCenter';AppId='37182072-3c9c-4f6a-a4b3-b3f91cacffce'},
